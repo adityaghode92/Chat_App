701e1fb343a6cb98bf060bc8ce718ddf6a1398e6
Adityaghode02 <adityaghode92@gmail.com>
2024-09-14 14:11:10 +0530

    my chat app overview

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { FaPenAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+	return (
+		<div className="w-full min-h-32  shadow-gray-950 shadow-inner flex flex-col justify-between items-start px-4 py-8 bg-slate-800 text-white">
+			<h1 className="font-bold text-lg flex items-center gap-4">
+				<span>Chat Application</span>
+				<FaPenAlt fontSize={16} />
+			</h1>
+			<div className="flex items-center justify-start w-full p-4 flex-wrap">
+				<div className="flex flex-col min-w-[280px] w-[33%] my-3">
+					<h1 className="font-semibold mb-2">Contact</h1>
+					<span>Aditya Ghode</span>
+					<span>pune</span>
+					<span>
+						<Link
+							to={"mailto:contact.adityaghode02@gmail.com"}
+							target="_blank"
+							className="hover:text-blue-600 hover:underline"
+						>
+							contact.adityaghode02@gmail.com
+						</Link>
+					</span>
+				</div>
+				<div className="flex flex-col min-w-[280px] w-[33%] my-3">
+					<h1 className="font-semibold mb-2">Pages</h1>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/"}
+						>
+							Chat App
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/signin"}
+						>
+							SignIn
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/signup"}
+						>
+							SignUp
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/home"}
+						>
+							Home
+						</Link>
+					</span>
+				</div>
+				<div className="flex flex-col min-w-[280px] w-[33%] my-3">
+					<h1 className="font-semibold mb-2">Links</h1>
+					<span>
+						<a
+							className="hover:text-blue-600 hover:underline"
+							href="https://www.linkedin.com/in/adityaghode92/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							LinkedIn
+						</a>
+					</span>
+					<span>
+						<a
+							className="hover:text-blue-600 hover:underline"
+							href="https://github.com/adityaghode92/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Github
+						</a>
+					</span>
+					<span>
+						<a
+							className="hover:text-blue-600 hover:underline"
+							href="https://instagram.com/aditya__0072/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Instagram
+						</a>
+					</span>
+					<span>
+						<a
+							className="hover:text-blue-600 hover:underline"
+							href="mailto:contact.adityaghode02@gmail.com"
+							target="_blank"
+							rel="noreferrer"
+						>
+							E-Mail
+						</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
